93095cffa9db84cba95b8dfc52fea6c035340cca
Cyrus Lau <cyruslau.work@gmail.com>
2023-10-31 17:04:16 +0800

    change timestampStart to 0

--- flutter_application_1/lib/services/http_service.dart
+++ flutter_application_1/lib/services/http_service.dart
@@ -2,12 +2,12 @@ import 'package:http/http.dart' as http;
 
 class HTTPService {
   Future<http.Response> fetchCSV() async {
-    var timestampStart = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-    var timestampEnd = DateTime(2023, 10, 1, 0, 0).millisecondsSinceEpoch ~/
-        1000; // DateTime(2017, 9, 7, 17, 30); 7th of September 2017, 5:30pm
+    var timestampStart = 0;
+    //  DateTime(2023, 10, 1, 0, 0).millisecondsSinceEpoch ~/1000; // DateTime(2017, 9, 7, 17, 30); 7th of September 2017, 5:30pm
+    var timestampEnd = DateTime.now().millisecondsSinceEpoch ~/ 1000;
     // Update the URL to point to the CSV file
     final url = Uri.parse(
-        'https://query1.finance.yahoo.com/v7/finance/download/SPY?period1=$timestampEnd&period2=$timestampStart&interval=1d&events=history&includeAdjustedClose=true');
+        'https://query1.finance.yahoo.com/v7/finance/download/SPY?period1=$timestampStart&period2=$timestampEnd&interval=1d&events=history&includeAdjustedClose=true');
 
     // Modify the request headers to accept CSV data
     final headers = {'Accept': 'text/csv'};
--- flutter_application_1/lib/services/time_service.dart
+++ flutter_application_1/lib/services/time_service.dart
@@ -1,10 +1,14 @@
 import 'package:intl/intl.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger();
 
 class TimeService {
   int convertToUnixTimestamp(String dateString) {
     final DateFormat format = DateFormat('yyyy-MM-dd');
     final DateTime date = format.parse(dateString);
     final int timestamp = date.millisecondsSinceEpoch ~/ 1000;
+    // logger.d(timestamp);
     return timestamp;
   }
 }
