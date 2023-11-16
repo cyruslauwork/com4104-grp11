3b5515f11e54d510f46b18faf691566aec75b63d
Cyrus Lau <cyruslau.work@gmail.com>
2023-11-16 11:07:18 +0800

    hotfix: CERTIFICATE_VERIFY_FAILED on Android

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,17 @@
 import 'bootstrap.dart';
 import 'services/services.dart';
+import 'dart:io';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   bootstrap(Flavor.prod, APIProvider.polygon);
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
