797ad9783f8aff08b04ed6f7888fd519b93097c2
Cyrus Lau <cyruslau.work@gmail.com>
2024-04-15 00:54:01 +0800

    font

---
+++ flutter_application_1/lib/styles/text.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/services/services.dart';
+
+import 'package:flutter_application_1/styles/styles.dart';
+import 'package:flutter_application_1/utils/utils.dart';
+
+extension CustomStyles on TextTheme {
+  TextStyle get sp3 => TextStyle(
+        color: ThemeColor.primary.value,
+        fontSize: 3.sp,
+        fontFamily: font,
+      );
+  TextStyle get sp5 => TextStyle(
+        color: ThemeColor.primary.value,
+        fontSize: 5.sp,
+        fontFamily: font,
+      );
+  TextStyle get sp7 => TextStyle(
+        color: ThemeColor.primary.value,
+        fontSize: 7.sp,
+        fontFamily: font,
+      );
+  TextStyle get sp10 => TextStyle(
+        color: ThemeColor.primary.value,
+        fontSize: 10.sp,
+        fontFamily: font,
+      );
+
+  String get font {
+    switch (LangService.to.currentLang) {
+      case Lang.en:
+        return 'OpenSans';
+      case Lang.zh:
+        return 'NotoSansHK';
+    }
+  }
+}
+
+extension CustomTextStyle on TextStyle {
+  TextStyle get w400 => copyWith(fontWeight: FontWeight.w400);
+  // TextStyle get w500 => copyWith(fontWeight: FontWeight.w500);
+  // TextStyle get w600 => copyWith(fontWeight: FontWeight.w600);
+  TextStyle get w700 => copyWith(fontWeight: FontWeight.w700);
+  // TextStyle get italic => copyWith(fontStyle: FontStyle.italic);
+  TextStyle get primaryTextColor => copyWith(color: AppColor.primaryTextColor);
+  // TextStyle get secondaryTextColor =>
+  //     copyWith(color: AppColor.secondaryTextColor);
+  // TextStyle get blackColor => copyWith(color: AppColor.blackColor);
+  // TextStyle get editTextColor => copyWith(color: AppColor.editTextColor);
+  // TextStyle get editTextErrorColor =>
+  //     copyWith(color: AppColor.editTextErrorColor);
+  // TextStyle get greyColor => copyWith(color: AppColor.greyColor);
+  TextStyle get underline => copyWith(decoration: TextDecoration.underline);
+}
--- flutter_application_1/lib/styles/theme.dart
+++ flutter_application_1/lib/styles/theme.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_application_1/utils/utils.dart';
-
 class AppColor {
   // Singleton implementation
   static final AppColor _instance = AppColor._();
@@ -35,40 +33,4 @@ extension ThemeColorExtension on ThemeColor {
         return Colors.yellow;
     }
   }
-}
-
-extension CustomStyles on TextTheme {
-  TextStyle get sp3 => TextStyle(
-        color: ThemeColor.primary.value,
-        fontSize: 3.sp,
-      );
-  TextStyle get sp5 => TextStyle(
-        color: ThemeColor.primary.value,
-        fontSize: 5.sp,
-      );
-  TextStyle get sp7 => TextStyle(
-        color: ThemeColor.primary.value,
-        fontSize: 7.sp,
-      );
-  TextStyle get sp10 => TextStyle(
-        color: ThemeColor.primary.value,
-        fontSize: 10.sp,
-      );
-}
-
-extension CustomTextStyle on TextStyle {
-  TextStyle get w400 => copyWith(fontWeight: FontWeight.w400);
-  // TextStyle get w500 => copyWith(fontWeight: FontWeight.w500);
-  // TextStyle get w600 => copyWith(fontWeight: FontWeight.w600);
-  TextStyle get w700 => copyWith(fontWeight: FontWeight.w700);
-  // TextStyle get italic => copyWith(fontStyle: FontStyle.italic);
-  TextStyle get primaryTextColor => copyWith(color: AppColor.primaryTextColor);
-  // TextStyle get secondaryTextColor =>
-  //     copyWith(color: AppColor.secondaryTextColor);
-  // TextStyle get blackColor => copyWith(color: AppColor.blackColor);
-  // TextStyle get editTextColor => copyWith(color: AppColor.editTextColor);
-  // TextStyle get editTextErrorColor =>
-  //     copyWith(color: AppColor.editTextErrorColor);
-  // TextStyle get greyColor => copyWith(color: AppColor.greyColor);
-  TextStyle get underline => copyWith(decoration: TextDecoration.underline);
 }
